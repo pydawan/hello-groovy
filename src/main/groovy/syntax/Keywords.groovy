7eb6a9f2b91f521d033ac1c76b86bdec83f34c72
Thiago Monteiro <thicmp@gmail.com>
2018-07-12 20:43:33 -0300

    Adicionada listagem de palavras-chave do Groovy.

---
+++ src/main/groovy/syntax/Keywords.groovy
@@ -0,0 +1,45 @@
+#!/usr/bin/env groovy
+
+//  /############################
+//  /   Groovy keywords         /
+// #############################/
+
+/*
+1. as
+2. assert
+3. break
+4. case
+5. catch
+6. class
+7. const
+8. continue
+9. def
+10. default
+11. do
+12. else
+13. enum
+14. extends
+15. false
+16. finally
+17. for
+18. goto
+19. if
+20. implements
+21. import
+22. in
+23. instanceof
+24. interface
+25. new
+26. null
+27. package
+28. return
+29. super
+30. switch
+31. this
+32. throw
+33. throws
+34. trait
+35. true
+36. try
+37. while
+
