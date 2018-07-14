0b876c465c5fa9802daac4294f7b6421a38bfd36
Thiago Monteiro <thicmp@gmail.com>
2018-07-14 12:10:06 -0300

    Adicionado exemplo do uso de tipo de dado lógico no Groovy.

---
+++ src/main/groovy/syntax/Booleans.groovy
@@ -0,0 +1,10 @@
+#!/usr/bin/env groovy
+
+/* Boolean is a special data type that is used to represent truth values: true and false. Use this data type for simples flags
+ * that track true/false conditions.
+ */
+
+def myBooleanVariable = true
+boolean untypedBooleanVar = false
+booleanField = true
+
